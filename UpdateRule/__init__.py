1a48a587ce44886c4e276b74ab19c360fe2a0fc7
Yuki Hattori <15963767+yuhattor@users.noreply.github.com>
2022-11-18 18:15:49 +0900

    Refactored api

--- UpdateRule/__init__.py
+++ UpdateRule/__init__.py
@@ -25,7 +25,7 @@ def main(req: func.HttpRequest, outdoc: func.Out[func.Document])  -> func.HttpRe
         user_rules = req.form['rules']
         mention = req.form['mention']
     except (KeyError, ValueError):  
-        logging.error('Json payload was not valid')
+        logging.error("Json payload was not valid")
         return func.HttpResponse("Sorry, something went wrong!", status_code=500)
 
 
@@ -33,7 +33,7 @@ def main(req: func.HttpRequest, outdoc: func.Out[func.Document])  -> func.HttpRe
     try:
         connection_string = os.environ['cosmosdb_connection_string']
     except (KeyError, ValueError):
-        logging.error('Could not read environment variables')
+        logging.error("Could not read environment variables")
         return func.HttpResponse("Sorry, something went wrong!", status_code=500)
 
     # Get org data from CosmosDB
@@ -55,12 +55,12 @@ def main(req: func.HttpRequest, outdoc: func.Out[func.Document])  -> func.HttpRe
                 "password_hash": existing_data["password_hash"]
             }
             outdoc.set(func.Document.from_json(json.dumps(outdata)))
-            return func.HttpResponse(f"Success!", status_code=200)
+            return func.HttpResponse("Success!", status_code=200)
         except:
-            logging.error('Could not store data in CosmosDB')
+            logging.error("Could not store data in CosmosDB")
             return func.HttpResponse("Sorry, something went wrong!", status_code=500)
     else:
-        return func.HttpResponse(f"You gave me a wrong password.", status_code=500)
+        return func.HttpResponse("You gave me a wrong password.", status_code=500)
 
 
 
